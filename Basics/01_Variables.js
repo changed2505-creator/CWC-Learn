1fdb8576201f23369afca31c05aa5881f00fe1ee
changed2505-creator <changed2505@gmail.com>
2026-06-18 17:09:19 +0000

    Learn 1

---
+++ Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "Tell123@gmail.com"
+var accountPass = "123"
+accountCity = "Banglore"
+let accountState;
+
+// accountId = 234
+
+accountEmail="Mw45@gmail.com"
+accountPass="3456"
+accountCity="Jaipur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState]);
