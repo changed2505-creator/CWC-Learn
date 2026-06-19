7bd30a30060f794638f518912684d5ef9149d047
changed2505-creator <changed2505@gmail.com>
2026-06-19 13:45:59 +0000

    Operations

--- Basics/03_conversion&operation.js
+++ Basics/03_conversion&operation.js
@@ -17,12 +17,25 @@
 // console.log(typeof data);
 // console.log(data);\
 
-let score = 23
+// let score = 23
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let data = String(score);
-console.log(typeof data);
-console.log(data);
+// let data = String(score);
+// console.log(typeof data);
+// console.log(data);
+
+// let a = "1"+2+2;
+// console.log(a,typeof a);
+// let b = 2+1+"2";
+// console.log(b,typeof b);
+
+
+// let n1,n2,n3;
+// n1=n2=n3=1+2; 
+
+let a = 24;
+console.log(a++);
+// console.log(++a);
 
