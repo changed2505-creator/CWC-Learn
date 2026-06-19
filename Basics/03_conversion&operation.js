23af4b9ac65eb00fab7abe46d1d2bcf01df33fd5
changed2505-creator <changed2505@gmail.com>
2026-06-19 12:22:56 +0000

    Conversion

---
+++ Basics/03_conversion&operation.js
@@ -0,0 +1,28 @@
+// let score = "Hi"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let data = Number(score);
+// console.log(typeof data);
+// console.log(data);
+
+
+// let score = "Hi"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let data = Boolean(score);
+// console.log(typeof data);
+// console.log(data);\
+
+let score = 23
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let data = String(score);
+console.log(typeof data);
+console.log(data);
+
