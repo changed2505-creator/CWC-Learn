87b37c10180ee7bfddf54dfafd9c44896e7f01d6
changed2505-creator <changed2505@gmail.com>
2026-06-20 07:57:37 +0000

    Strings and Num

---
+++ Basics/06_Srings.js
@@ -0,0 +1,34 @@
+// let newStr = "Hi"
+// let str = new String("Hello")
+
+// console.log(newStr);
+// console.log(str);
+
+// console.log(str[1]);
+// console.log(str.__proto__);
+
+const str = "Hippopotamus-Water"
+
+// console.log(str.charAt(5));
+// console.log(str.indexOf('t'));
+
+// const newStr = str.substring(0,5);
+// console.log(newStr);
+
+// const anotherStr = str.slice(-8,12);
+// console.log(anotherStr);
+
+// const newStr = "           Hi-Bye-Kadle-kai    ";
+// console.log(newStr);
+// console.log(newStr.trim());
+
+// const newStr = str.replace("-",",");
+// console.log(newStr);
+
+// const newStr = str.includes('at');
+// console.log(newStr);
+
+const newStr = str.split("-",2);
+console.log(newStr,typeof(newStr));
+
+ 
---
+++ Basics/07_Nums&Maths.js
@@ -0,0 +1,14 @@
+// const numOne = 100;
+// const numTwo = new Number(150)
+
+// console.log(numOne);
+// console.log(numTwo.toString());
+
+// const numThree = 102.3456
+// console.log(numThree.toFixed(3));
+// console.log(numThree.toPrecision(3));
+
+
+
+
+
