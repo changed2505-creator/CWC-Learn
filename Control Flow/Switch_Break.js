a91a1b0bd8f5ce65fe735b335e32109e9b5ad566
changed2505-creator <changed2505@gmail.com>
2026-06-22 11:12:09 +0000

    Control flow in JS

---
+++ Control Flow/Switch_Break.js
@@ -0,0 +1,47 @@
+// const day = 5
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+
+//     default:
+//         console.log("Enter day values of range 1-7");
+
+//         break;
+// }
+
+
+// let arr = [1];
+// let obj = {a:1};
+
+// if(arr.length==0){
+//     console.log("Empty array");    
+// }
+// if(Object.keys(obj).length==0){
+//     console.log("Empty object");    
+// }
+
+let val1;
+
+// val1=null??10??20;
+val1=undefined??10??20;
+
+console.log(val1);
