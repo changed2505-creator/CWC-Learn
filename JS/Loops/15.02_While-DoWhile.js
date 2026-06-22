8d6b9b862b7e755c1e32254ac5dfbbf406a05f00
changed2505-creator <changed2505@gmail.com>
2026-06-22 14:53:34 +0000

    Map and For-of loop

---
+++ JS/Loops/15.02_While-DoWhile.js
@@ -0,0 +1,19 @@
+let a = 5;
+while (a<12) {
+    // console.log(`Value : ${a}`);    
+    a++ 
+}
+
+let arr = [1,2,3,4,5];
+let index = 3;
+while (index<arr.length) {
+    // console.log(`Value : ${arr[index]}`);
+    index++
+}
+
+let i=11;
+do {
+    console.log(i);
+    i++
+} while (i<10);
+
---
+++ JS/Loops/15.03_ForOf-Maps.js
@@ -0,0 +1,17 @@
+let arr = [1,2,3,4,5]
+
+for (const i of arr) {
+    // console.log(i)
+}
+
+let map = new Map();
+map.set("a",1);
+map.set('b',2);
+// map.set('a',4);
+
+// console.log(map);
+
+for (const [key,value] of map) {
+    // console.log(key);    
+}
+
